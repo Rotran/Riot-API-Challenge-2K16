9925103a766d891284b5296c8fda01ad18a60f82
Wesley <wesleyrogers09@gmail.com>
2016-04-28 21:36:20 -0500

    Simple api functionality :dragon:

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var r = require('rethinkdbdash')();
+
+
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    fs.readFile('public/index.html', 'utf8', function(err, data){
-        if(err) throw err;
+    fs.readFile('public/index.html', 'utf8', function (err, data) {
+        if (err) {
+            throw err;
+        }
         res.write(data);
         res.end;
     });
 });
 
-app.listen(3000, function(){
+app.get('/data', function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+    r.db('LoL').table('Summoners').run().then(function (data) {
+        console.log(data);
+        res.write(JSON.stringify({
+            "results": data
+        }));
+        res.end;
+    });
+})
+
+app.listen(3000, function () {
     console.log('Server listening on port 3000');
 });
