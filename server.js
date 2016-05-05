ab32f95de0c663fa158bd4845f5353e84d7e75cf
Wesley <wesleyrogers09@gmail.com>
2016-05-04 20:07:17 -0500

    Adding temp file for key use (ignored) and updates to the server.

--- server.js
+++ server.js
@@ -27,14 +27,14 @@ app.get('/', function (req, res) {
 });
 
 //Gathers the data from our database
-app.get('/data', function (req, res) {
+app.get('/LCS', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     r.db('LoL').table('Summoners').run().then(function (data) {
         console.log(data);
         //When sending actual data, don't use stringify!!
         res.send(JSON.stringify({
-            "results": data
-        }, null, 4));
+            data
+        }, null, 3));
     });
 });
 
@@ -42,18 +42,19 @@ app.get('/data', function (req, res) {
 app.get('/temp', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
 
-    fs.readFile('public/../players.json', 'utf8', function(err, data){
-        if(err){
+    fs.readFile('public/../players.json', 'utf8', function (err, data) {
+        if (err) {
             throw err;
         }
         var parsed = JSON.parse(data);
-
-        for(var i=0; i < parsed['Players'].length; i++){
+        var payload = '';
+        console.log(parsed['Players'][5]['Player']);
+        for (var i = 0; i < parsed['Players'].length; i++) {
             var j = parsed['Players'][i];
             console.log('Player: ' + j['Player']);
-            res.write("Player: ")
+            payload += j['Player'] + ', ';
         }
-
+        res.write(payload);
         //res.send(parsed);
     });
 });
@@ -63,33 +64,19 @@ app.get('/user/:name', function (req, res) {
     var name = req.params.name;
     console.log("getting user " + req.params.name);
     res.setHeader('Content-Type', 'application/json');
-    request.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + name + "?api_key=" + key, function(data,
-    response, body){
+    request.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + name + "?api_key=" + key, function (data,
+        response, body) {
         res.send(JSON.parse(body));
-    }).on("error", function(e){
+    }).on("error", function (e) {
         console.log("Error: " + e.message);
     });
 });
 
-//Schema used for the prompt
-var schema = {
-    properties:{
-        key:{
-            message: "Please enter in riot key: ",
-            required: true
-        }
-    }
-};
 
-//Prompt for the API key and start the server
-prompt.get(schema, function(err, result){
-    key = result.key;
-    console.log('key: ' + key.toString());
-    app.listen(3000, function(){
-        console.log('Server listening on port 3000');
+app.listen(3000, function () {
+    console.log('Server listening on port 3000');
+    fs.readFile('key.config', 'utf8', function (err, data) {
+        console.log(data);
+        key = data;
     });
-})
-
-
-
-
+});
